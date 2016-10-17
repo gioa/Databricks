4b45f8f4e2019b1b41569cf44d4b902456054e31
gioa <joy@databricks.com>
2016-10-17 16:20:25 -0700

    Update TestGithubLink.py

--- notebooks/Users/joy@databricks.com/TestGithubLink.py
+++ notebooks/Users/joy@databricks.com/TestGithubLink.py
@@ -1,12 +1,10 @@
-# Databricks notebook source exported at Mon, 17 Oct 2016 22:06:24 UTC
+# Databricks notebook source exported at Mon, 17 Oct 2016 23:20:24 UTC
 # MAGIC %md test link github
 
 # COMMAND ----------
 
 print "I update from github"
 print "After editing from notebook, another update from Github"
-print "will this commit in Github generate a new revision item?"
-
 
 # COMMAND ----------
 
